b8f9cb68e017d9d9b49927c7f5d63a59454da6ad
fernando <fjanuarioazevedo@gmail.com>
2017-01-17 23:14:48 -0300

    ajustes

--- cumurupraias/urls.py
+++ cumurupraias/urls.py
@@ -61,9 +61,3 @@ if settings.DEBUG:
     )
 
 
-
-from django.conf.urls import url, include
-from django.contrib import admin
-from django.conf import settings
-from django.views.static import serve as serve_static
-from django.conf.urls.static import static
--- home/models.py
+++ home/models.py
@@ -9,6 +9,8 @@ class Home(models.Model):
     link = models.CharField('link',max_length=120, blank=True)
     palavralink = models.CharField('palavra-link',max_length=120, blank=True)
     subtitle = models.CharField('Legenda', max_length=100, blank=True)
+    subtitlelink = models.CharField('subtitle-link',max_length=120, blank=True)
+    subtitlepalavralink = models.CharField('palavra-subtitle-link',max_length=120, blank=True)
     content = models.TextField('Conteudo', blank=True)
     created = models.DateTimeField('Criado em', auto_now_add=True)
     modified = models.DateTimeField('Modificado em', auto_now=True)
